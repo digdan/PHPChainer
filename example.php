fa71e28748d479dc415afe12cbb08b4fe6d4bb8d
digdan <dan@danmorgan.net>
2013-07-10 13:21:45 -0700

    Update example.php

--- example.php
+++ example.php
@@ -1,37 +1,5 @@
 <?
-class C {
-	static $registry=array();
-	static $pointer;
-
-	static function chain() {
-		return new static;
-	}
-
-	public static function register($obj,$name) {
-		self::$registry[$name] = $obj;	
-		self::$pointer = $name;
-		return new static;
-	}
-
-	public function __call($name, $arguments) {
-		$target = array(self::$registry[self::$pointer],$name);
-		if (is_object(self::$registry[self::$pointer])) {
-			if (is_callable( $target , true )) {
-				call_user_func_array ( $target , $arguments );
-			}
-		}
-		return new static;
-	}
-
-	public function point($name) {
-		$target = array(self::$registry[self::$pointer],$name);
-		if (is_object(self::$registry[self::$pointer])) {
-			self::$pointer = $name;
-		}
-		return new static;
-	}
-}
-
+include_once("chainer.php");
 
 class fruit {
 	var $type = "fruit";
